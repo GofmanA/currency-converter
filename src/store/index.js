6d5f0e8ebe9981402424c4edc01748a41ba75251
gdeMojChay <thetypgame@gmail.com>
2021-10-24 02:09:16 +0400

    setting up vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    api_key: 'mxqiSXJxXRLQ9fmfH7UcvsJvdsbxSg',
+    base_currency: 'RUB'
   },
   mutations: {
+    SET_BASE: (state, payload) => {
+      state.base_currency = payload
+    }
+  },
+  getters: {
+    API: state => {
+      return state.api_key
+    },
+    BASE: state => {
+      return state.base_currency
+    }
   },
   actions: {
+    SET_BASE: (injectee, payload) =>  {
+      injectee.commit('SET_BASE', payload)
+    }
   },
   modules: {
   }
